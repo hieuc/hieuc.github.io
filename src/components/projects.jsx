11f9a3e03a9d9784fef1634336207925fb160a6c
hieuc <hieu2192012@gmail.com>
2022-09-28 16:25:25 -0700

    added modal for project extra infos

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,53 +1,92 @@
-import React from "react"
+import React, { useState } from "react"
 import "../styles/projects.css"
-import { FaGithub } from "react-icons/fa"
+import { FaGithub, facross } from "react-icons/fa"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Projects = () => {
   const data = useStaticQuery(query)
+  // project that being expanded for more info.
+  // index starts from 0, -1 is the inactive state
+  const [active, setActive] = useState(-1)
 
   return (
     <div id="projects-container">
       {data.dataJson.content.map((e, i) => {
         return (
-          <div className="row">
-            <div
-              key={i}
-              className="col-lg-6 d-flex align-items-stretch card-main"
-            >
-              <div className="card">
-                <img
-                  className="card-img"
-                  src={e.demoImage.publicURL}
-                  alt={e.name}
-                />
-                <div className="card-body">
-                  <div className="card-title">
-                    {e.name}{" "}
-                    {e.github ? (
-                      <a href={e.github} target="_blank" rel="noreferrer">
-                        <FaGithub className="fa" />
-                      </a>
-                    ) : null}
+          <>
+            <div className="row">
+              <div
+                key={"projects" + i}
+                className="col-lg-6 d-flex align-items-stretch card-main"
+              >
+                <div className="card">
+                  <img
+                    className="card-img"
+                    src={e.demoImage.publicURL}
+                    alt={e.name}
+                    onClick={() => {
+                      setActive(i)
+                    }}
+                  />
+                  <div className="card-body">
+                    <div className="card-title">
+                      {e.name}{" "}
+                      {e.github ? (
+                        <a href={e.github} target="_blank" rel="noreferrer">
+                          <FaGithub className="fa" />
+                        </a>
+                      ) : null}
+                    </div>
+                    <div className="second-title">{e.tech}</div>
+                    <p className="card-text">{e.description}</p>
                   </div>
-                  <div className="second-title">{e.tech}</div>
-                  <p className="card-text">{e.description}</p>
-                </div>
-                <div>
-                  {e.links.map(link => (
-                    <a
-                      className="btn"
-                      href={link.link}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {link.name}
-                    </a>
-                  ))}
+                  <div>
+                    {e.links.map((link, li) => (
+                      <a
+                        className="btn"
+                        href={link.link}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={"link" + e.name + li}
+                      >
+                        {link.name}
+                      </a>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+
+            <div
+              id="modal"
+              style={{ display: active === i ? "grid" : "none" }}
+              onClick={() => {
+                setActive(-1)
+              }}
+              key={"modal" + i}
+            >
+              <div
+                className="modal-card"
+                onClick={e => {
+                  e.stopPropagation()
+                }}
+              >
+                <div className="modal-header">
+                  <div
+                    className="xmark"
+                    onClick={() => {
+                      setActive(-1)
+                    }}
+                  ></div>
+                  {e.name}
+                </div>
+                <div
+                  className="modal-content"
+                  dangerouslySetInnerHTML={{ __html: e.html }}
+                ></div>
+              </div>
+            </div>
+          </>
         )
       })}
     </div>
@@ -69,6 +108,7 @@ const query = graphql`
           name
           link
         }
+        html
       }
     }
   }
